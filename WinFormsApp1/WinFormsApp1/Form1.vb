1735b406deaef0bd8e03608ee3401e305eea40a1
profeale2023 <quintoseptimaet@gmail.com>
2022-08-09 20:34:03 -0300

    segunda subida

---
+++ WinFormsApp1/WinFormsApp1/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+        Form2.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Hide()
+        Form2.Show()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Show()
+        Form2.Show()
+
+        Me.Left = 220
+        Me.Top = 220
+
+    End Sub
+End Class
---
+++ WinFormsApp1/WinFormsApp1/Form2.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form2
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+        Form1.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Hide()
+        Form1.Show()
+    End Sub
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Left = 220 + Form1.Width
+        Me.Top = 220
+    End Sub
+End Class
